0f844d13999d9efee383a36eb9354604d43de32c
kennedy virus <jfkvirus@gmail.com>
2019-09-30 11:41:39 +0000

    =Redesign dashboard slider

--- resources/views/espaces/admin/home.blade.php
+++ resources/views/espaces/admin/home.blade.php
@@ -1,223 +1,39 @@
 @extends("default")
 @section('css')
-<style>
-
-    *{box-sizing:border-box}
-
-    /* Slideshow container */
-    .slideshow-container {
-        max-width: 1000px;
-        /*max-height: 400px !important;*/
-        position: relative;
-        margin: auto;
-    }
-
-    /* Hide the images by default */
-    .mySlides {
-        display: none;
-    }
-
-    /* Next & previous buttons */
-    .prev, .next {
-        cursor: pointer;
-        position: absolute;
-        top: 50%;
-        width: auto;
-        margin-top: -22px;
-        padding: 16px;
-        color: white;
-        font-weight: bold;
-        font-size: 18px;
-        transition: 0.6s ease;
-        border-radius: 0 3px 3px 0;
-        user-select: none;
-    }
-
-    /* Position the "next button" to the right */
-    .next {
-        right: 0;
-        border-radius: 3px 0 0 3px;
-    }
-
-    /* On hover, add a black background color with a little bit see-through */
-    .prev:hover, .next:hover {
-        background-color: rgba(0,0,0,0.8);
-    }
-
-    /* Caption text */
-    .text {
-        color: #f2f2f2;
-        font-size: 15px;
-        padding: 8px 12px;
-        position: absolute;
-        bottom: 8px;
-        width: 100%;
-        text-align: center;
-    }
-
-    /* Number text (1/3 etc) */
-    .numbertext {
-        color: #f2f2f2;
-        font-size: 12px;
-        padding: 8px 12px;
-        position: absolute;
-        top: 0;
-    }
-
-    /* The dots/bullets/indicators */
-    .dot {
-        cursor: pointer;
-        height: 15px;
-        width: 15px;
-        margin: 0 2px;
-        background-color: #bbb;
-        border-radius: 50%;
-        display: inline-block;
-        transition: background-color 0.6s ease;
-    }
-
-    .active, .dot:hover {
-        background-color: #717171;
-    }
-
-    /* Fading animation */
-    .fade {
-        -webkit-animation-name: fade;
-        -webkit-animation-duration: 1.5s;
-        animation-name: fade;
-        animation-duration: 1.5s;
-    }
-
-    @-webkit-keyframes fade {
-        from {opacity: .4}
-        to {opacity: 1}
-    }
-
-    @keyframes fade {
-        from {opacity: .4}
-        to {opacity: 1}
-    }
-
-    /*.slider .indicators .indicator-item {*/
-        /*background-color: #666666;*/
-        /*-webkit-box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);*/
-        /*-moz-box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);*/
-        /*box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);*/
-    /*}*/
-    /*.slider .indicators .indicator-item.active {*/
-        /*background-color: #ffffff;*/
-    /*}*/
-    /*.slider {*/
-        /*width: 100%;*/
-        /*margin: 0 auto;*/
-        /*height: 800px;*/
-    /*}*/
-    /*.slider .indicators {*/
-        /*bottom: 60px;*/
-        /*z-index: 100;*/
-        /*!* text-align: left; *!*/
-    /*}*/
-
-</style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
 @endsection
 
 @section('js')
-    <script type="text/javascript">
-        var slideIndex = 0;
-        showSlides();
-
-        function showSlides() {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > slides.length) {slideIndex = 1}
-            slides[slideIndex-1].style.display = "block";
-            setTimeout(showSlides, 3000); // Change image every 2 seconds
-        }
-    </script>
-    {{--<script type="text/javascript">--}}
-        {{--var slideIndex = 1;--}}
-        {{--showSlides(slideIndex);--}}
-
-        {{--function plusSlides(n) {--}}
-            {{--showSlides(slideIndex += n);--}}
-        {{--}--}}
-
-        {{--function currentSlide(n) {--}}
-            {{--showSlides(slideIndex = n);--}}
-        {{--}--}}
-
-        {{--function showSlides(n) {--}}
-            {{--var i;--}}
-            {{--var slides = document.getElementsByClassName("mySlides");--}}
-            {{--var dots = document.getElementsByClassName("dot");--}}
-            {{--if (n > slides.length) {slideIndex = 1}--}}
-            {{--if (n < 1) {slideIndex = slides.length}--}}
-            {{--for (i = 0; i < slides.length; i++) {--}}
-                {{--slides[i].style.display = "none";--}}
-            {{--}--}}
-            {{--for (i = 0; i < dots.length; i++) {--}}
-                {{--dots[i].className = dots[i].className.replace(" active", "");--}}
-            {{--}--}}
-            {{--slides[slideIndex-1].style.display = "block";--}}
-            {{--dots[slideIndex-1].className += " active";--}}
-        {{--}--}}
-        {{--setTimeout(showSlides, 4000);--}}
-    {{--</script>--}}
-
 @endsection
 
 @section('content')
 
     <div id="homes">
-        <div class="m-t-lg">
-            <div class="row">
+        <div class="m-t-lg" id="slideBox">
+            <div class="slider">
+                <ul class="slider-parent">
+                    <li class="images-list" data-slider="1">
+                        <img src="{{asset('assets/img/new CLP/14.jpg')}}">
+                    </li>
+                    <li class="images-list" data-slider="2">
+                        <img src="{{asset('assets/img/new CLP/14.jpg')}}">
+                    </li>
+                    <li class="images-list" data-slider="3">
+                        <img src="{{asset('assets/img/new CLP/14.jpg')}}">
+                    </li>
+                </ul>
+                <ol class="buttom-circles">
+                    <li class="buttom-circles-list slider-active" data-slider="1"><i class="fa fa-circle-thin"></i></li>
+                    <li class="buttom-circles-list" data-slider="2"><i class="fa fa-circle-thin"></i></li>
+                    <li class="buttom-circles-list" data-slider="3"><i class="fa fa-circle-thin"></i></li>
 
-                {{--<div id="slider_container">--}}
-                    <!-- Slideshow container -->
-                    <div class="slideshow-container">
-
-                        <!-- Full-width images with number and caption text -->
-                        <div class="mySlides fade">
-                            {{--<div class="numbertext">1 / 3</div>--}}
-                            <img src="{{asset('assets/img/new CLP/14.jpg')}}" style="width:100%">
-                        </div>
-
-                        <div class="mySlides fade">
-                            <img src="{{asset('assets/img/new CLP/11.jpg')}}" style="width:100%">
-                        </div>
-
-                        <div class="mySlides fade">
-                            <img src="{{asset('assets/img/new CLP/15.jpg')}}" style="width:100%">
-                        </div>
-
-                        <!-- Next and previous buttons -->
-                        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                    </div>
-                    <br>
-
-                    <!-- The dots/circles -->
-                    <div style="text-align:center">
-                        <span class="dot" onclick="currentSlide(1)"></span>
-                        <span class="dot" onclick="currentSlide(2)"></span>
-                        <span class="dot" onclick="currentSlide(3)"></span>
-                    </div>
-
-                </div>
+                </ol>
+                {{--<i class="fa fa-chevron-right fa-5x"></i>--}}
+                {{--<i class="fa fa-chevron-left fa-5x"></i>--}}
+            </div>
 
         </div>
-
-        {{--<div class="hidden-xs">--}}
-            {{--<img class="img-thumbnail" style="" src="{{asset('images/welcome.jpg')}}"/>--}}
-        {{--</div>--}}
-
-        {{--<div class="hidden-lg hidden-md hidden-sm">--}}
-            {{--<img class="img-thumbnail" style="" src="{{asset('images/welcomex.jpg')}}"/>--}}
-        {{--</div>--}}
 
         <div class="m-t-lg">
             <div class="row">
@@ -230,7 +46,6 @@
                             <div class="col-lg-9 col-md-8 col-sm-8 col-xs-8 fontsty">
                                 <h4>Professeurs</h4>
                                 <Strong><h1 style="font-weight: 600;text-align: right;margin-right: 30px;" id="profs"></h1></strong>
-                                {{--<Strong><h1 style="font-weight: 600;text-align: right;margin-right: 30px;">@{{ profs }}</h1></strong>--}}
                             </div>
                         </div>
                     </div>
@@ -245,7 +60,6 @@
                             <div class="col-lg-9 col-md-8 col-sm-8 col-xs-8 fontsty">
                                 <h4>Elèves</h4>
                                 <Strong><h1 style="font-weight: 600;text-align: right;margin-right: 30px;" id="eleves"></h1></strong>
-                                {{--<Strong><h1 style="font-weight: 600;text-align: right;margin-right: 30px;">@{{ eleves }}</h1></strong>--}}
                             </div>
                         </div>
                     </div>
@@ -259,7 +73,6 @@
                             <div class="col-lg-9 col-md-8 col-sm-8 col-xs-8 fontsty">
                                 <h4>Parents</h4>
                                 <Strong><h1 style="font-weight: 600;text-align: right;margin-right: 30px;" id="parents"></h1></strong>
-                                {{--<Strong><h1 style="font-weight: 600;text-align: right;margin-right: 30px;">@{{ parents }}</h1></strong>--}}
                             </div>
                         </div>
                     </div>
@@ -267,9 +80,136 @@
             </div>
         </div>
     </div>
-    <script type="text/javascript" src="{{asset('js/vues/admin/home.js')}}"></script>
-    {{--<script type="module" src="{{asset('js/vues/admin/home.js')}}"></script>--}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript">
 
-{{--<home></home>--}}
+        $(document).ready(function() {
+            $(function () {
+                "use strict";
+
+                var slider          = $('.slider'),
+                    sliderUl        = slider.find('.slider-parent'),
+                    sliderUlLi      = sliderUl.find('.images-list'),
+                    sliderOl        = slider.find('.buttom-circles'),
+                    sliderOlLi      = sliderOl.find('.buttom-circles-list'),
+                    sliderFaRight   = slider.find('> .fa:first-of-type'),
+                    sliderFaLeft    = slider.find('> .fa:last-of-type'),
+                    sliderTime      = 1000,
+                    sliderWait      = 2000,
+                    sliderSetInt,
+                    resumeAndPause;
+
+                sliderFaLeft.fadeOut();
+                var parents=0
+                var profs=0
+                var eleves=0
+
+                $('#parents').html(parents)
+                $('#profs').html(profs)
+                $('#eleves').html(eleves)
+
+                function resetWH() {
+                    slider.width(slider.parent().width()).height(slider.parent().width() * 0.5);
+                    sliderUl.width(slider.width() * sliderUlLi.length).height(slider.height());
+                    sliderUlLi.width(slider.width()).height(slider.height());
+                }
+                resetWH();
+
+                function runSlider() {
+                    if (sliderOlLi.hasClass('slider-active')) {
+                        sliderUl.animate({
+                            marginLeft: -slider.width() * ($('.slider-active').data('slider') - 1)
+                        }, sliderTime);
+                    }
+                    if ($('.slider-active').is(':first-of-type')) {
+                        sliderFaLeft.fadeOut();
+                    } else {
+                        sliderFaLeft.fadeIn();
+                    }
+                    if ($('.slider-active').next().is(':last-of-type')) {
+                        sliderFaRight.fadeOut();
+                    } else {
+                        sliderFaRight.fadeIn();
+                    }
+                }
+
+                function runRight() {
+                    slider.each(function () {
+                        $('.slider-active').next().addClass('slider-active').siblings().removeClass('slider-active');
+                        runSlider();
+                    });
+                }
+
+                function runLeft() {
+                    slider.each(function () {
+                        $('.slider-active').prev().addClass('slider-active').siblings().removeClass('slider-active');
+                        runSlider();
+                    });
+                }
+
+                sliderSetInt = function autoRunSlider() {
+                    if ($('.slider-active').next().is(':last-of-type')) {
+                        sliderUl.animate({
+                            marginLeft: -sliderUlLi.width() * $('.slider-active').data('slider')
+                        }, sliderTime, function () {
+                            sliderUl.css('margin-left', 0);
+                            sliderOlLi.first().addClass('slider-active').siblings().removeClass('slider-active');
+                        });
+                    } else {
+                        runRight();
+                    }
+                };
+
+                resumeAndPause = setInterval(sliderSetInt, sliderWait);
+
+
+                $(window).on('resize', function () {
+                    resetWH();
+                });
+
+
+                slider.each(function () {
+                    sliderOlLi.click(function () {
+                        $(this).addClass('slider-active').siblings().removeClass('slider-active');
+                        runSlider();
+                    });
+                });
+
+                sliderFaRight.on('click', function () {
+                    runRight();
+                });
+                sliderFaLeft.on('click', function () {
+                    runLeft();
+                });
+
+                slider.find('.fa').hover(function () {
+                    clearInterval(resumeAndPause);
+                }, function () {
+                    resumeAndPause = setInterval(sliderSetInt, sliderWait);
+                });
+
+                var request = $.ajax({
+                    url: "/ajax/load_admin_home",
+                    type: "GET",
+                   // dataType: "html"
+                });
+
+                request.done(function(response) {
+                    console.log(response)
+                    parents=response.parents
+                    profs=response.profs
+                    eleves=response.eleves
+
+                    $('#parents').html(parents)
+                    $('#profs').html(profs)
+                    $('#eleves').html(eleves)
+                });
+
+                request.fail(function(jqXHR, textStatus) {
+                    console.log("Request failed: " + textStatus );
+                });
+            });
+        });
+    </script>
 
 @endsection
